b46dcd31f1c724e4bec51157caeb0b3cea0a4793
Animal4Instinct <nico_r95@outlook.com>
2024-06-18 18:30:22 -0300

    Agregados css html y ts  de Home
    Carga los datos y hay filtro de busqueda

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,10 +1,157 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { RepairsService } from 'src/app/services/repairs.service';
+import { Router } from '@angular/router';
+import { TecnicsService } from 'src/app/services/tecnics.service';
+import { ClientsService } from 'src/app/services/clients.service';
+import { EquipoService } from 'src/app/services/equipo.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+ reparaciones: any[] = [];
+  tecnicos: any[] = [];
+  equipos: any[] = [];
+  clientes: any[] = [];
+  reparacionSeleccionada: any = {};
+  searchTerm: string = '';
+  estadoFiltro: string = 'Todos';
+  estados: string[] = ['En taller', 'Finalizada', 'Entregada'];
+  nuevaReparacion: any = {
+    fechaIngreso: '',
+    tecnico: { id: '' },
+    cliente: { id: '' },
+    equipo: { id: '' },
+    accesorios: '',
+    falla: '',
+    codigoSeguimiento: 'asdasdasd',
+    estado: '',
+    manoDeObra: 0,
+    entrega: 0,
+    saldo: 0,
+  };
+  reparacion: any = {
+    fechaIngreso: '',
+    tecnico: { id: '' },
+    cliente: { id: '' },
+    equipo: { id: '' },
+    accesorios: '',
+    falla: '',
+    codigoSeguimiento: '',
+    estado: '',
+    manoDeObra: 0,
+    entrega: 0,
+    saldo: 0,
+  };
 
+  errorAgregarReparacion = false;
+
+  constructor(
+    private RepairsService: RepairsService,
+    private TecnicsService: TecnicsService,
+    private ClientsService: ClientsService,
+    private EquipoService: EquipoService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.setFechaActual();
+    this.obtenerReparaciones();
+    this.obtenerTecnicos();
+    this.obtenerClientes();
+    this.obtenerEquipos();
+  }
+
+  obtenerReparaciones(): void {
+    this.RepairsService.getReparaciones().subscribe(
+      (data) => {
+        this.reparaciones = data;
+        console.log(this.reparaciones);
+      },
+      (error) => {
+        console.error('Error al obtener reparaciones:', error);
+      }
+    );
+  }
+
+  eliminarReparacion(id: number): void {
+    this.RepairsService.eliminarReparacion(id).subscribe(
+      () => {
+        this.obtenerReparaciones();
+      },
+      (error) => {
+        console.error('Error al eliminar reparacion', error);
+      }
+    );
+  }
+
+  seleccionaReparacion(reparacion: any): void {
+    this.reparacionSeleccionada = { ...reparacion };
+  }
+
+  obtenerClientes(): void {
+    this.ClientsService.getClientes().subscribe(
+      (data) => {
+        this.clientes = data;
+      },
+      (error) => {
+        console.error('Error al obtener clientes:', error);
+      }
+    );
+  }
+
+  obtenerTecnicos(): void {
+    this.TecnicsService.getTecnicos().subscribe(
+      (data) => {
+        this.tecnicos = data;
+        console.log(this.tecnicos);
+      },
+      (error) => {
+        console.error('Error al obtener los tecnicos:', error);
+      }
+    );
+  }
+
+  obtenerEquipos(): void {
+    this.EquipoService.getEquipos().subscribe(
+      (data) => {
+        this.equipos = data;
+        console.log(this.equipos);
+      },
+      (error) => {
+        console.error('Error al obtener los equipos:', error);
+      }
+    );
+  }
+
+  get filteredReparaciones() {
+    return this.reparaciones.filter((reparacion) =>
+      reparacion.cliente.nombre
+        .toLowerCase()
+        .includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  get filteredReparacionesForStatus() {
+    return this.reparaciones.filter((reparacion) => {
+      const matchesCliente = reparacion.cliente.nombre
+        .toLowerCase()
+        .includes(this.searchTerm.toLowerCase());
+      const matchesEstado =
+        this.estadoFiltro === 'Todos' ||
+        reparacion.estado === this.estadoFiltro;
+      return matchesCliente && matchesEstado;
+    });
+  }
+
+  setFechaActual(): void {
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const year = today.getFullYear();
+    this.nuevaReparacion.fechaIngreso = `${year}-${month}-${day}`;
+  }
 }
+
