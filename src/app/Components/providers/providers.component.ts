d7c420d05a983182125e65f0ae3360ce430320e5
STPR2001 <santiagoperron2016@gmail.com>
2024-07-17 21:20:59 -0300

    se agrego conirmacion antes de eliminar elemento

--- src/app/Components/providers/providers.component.ts
+++ src/app/Components/providers/providers.component.ts
@@ -4,11 +4,13 @@ import { Router } from '@angular/router';
 import { tap, catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ConfirmDialogComponent } from 'src/app/confirm-dialog/confirm-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-providers',
   templateUrl: './providers.component.html',
-  styleUrls: ['./providers.component.css']
+  styleUrls: ['./providers.component.css'],
 })
 export class ProvidersComponent implements OnInit {
   @ViewChild('agregarProveedorModal') modalCloseAdd: any;
@@ -26,22 +28,29 @@ export class ProvidersComponent implements OnInit {
   totalPages: number = 0;
   nombre: string = '';
 
-  constructor(private providersService: ProvidersService, private router: Router, private modalService: NgbModal) { }
+  constructor(
+    private providersService: ProvidersService,
+    private router: Router,
+    private modalService: NgbModal,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.getProveedores();
   }
 
   getProveedores(): void {
-    this.providersService.getProveedores(this.currentPage, this.pageSize, this.nombre).subscribe(
-      (data) => {
-        this.proveedores = data.content;
-        this.totalPages = data.totalPages;
-      },
-      (error) => {
-        console.error('Error al obtener la lista de proveedores:', error);
-      }
-    );
+    this.providersService
+      .getProveedores(this.currentPage, this.pageSize, this.nombre)
+      .subscribe(
+        (data) => {
+          this.proveedores = data.content;
+          this.totalPages = data.totalPages;
+        },
+        (error) => {
+          console.error('Error al obtener la lista de proveedores:', error);
+        }
+      );
   }
 
   onPageChange(page: number): void {
@@ -58,62 +67,80 @@ export class ProvidersComponent implements OnInit {
   }
 
   navigateToUpdateProveedor(id: string, nombre: string): void {
-    this.router.navigateByUrl(`/proveedores/update`, { state: { nombre: nombre, id: id } });
+    this.router.navigateByUrl(`/proveedores/update`, {
+      state: { nombre: nombre, id: id },
+    });
   }
 
   eliminarProveedor(id: number): void {
-    this.providersService.eliminarProveedor(id).subscribe(
-      () => {
-        this.getProveedores();
-      },
-      (error) => {
-        console.error('Error al eliminar proveedor:', error);
+    const dialogRef = this.dialog.open(ConfirmDialogComponent);
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.providersService.eliminarProveedor(id).subscribe(
+          () => {
+            this.getProveedores();
+          },
+          (error) => {
+            console.error('Error al eliminar proveedor:', error);
+          }
+        );
       }
-    );
+    });
   }
 
   agregarProveedor(): void {
-    this.providersService.agregarProveedor(this.nuevoProveedor).pipe(
-      tap(() => {
-        this.router.navigate(['/proveedores']);
-        this.nuevoProveedor = {};
-        this.getProveedores();
-        this.modalCloseAdd.nativeElement.click();
-      }),
-      catchError((error) => {
-        console.error('Error al agregar proveedor:', error);
-        this.errorAgregarProveedor = true;
-        setTimeout(() => {
-          this.errorAgregarProveedor = false;
-        }, 5000);
-        return of(error);
-      })
-    ).subscribe();
+    this.providersService
+      .agregarProveedor(this.nuevoProveedor)
+      .pipe(
+        tap(() => {
+          this.router.navigate(['/proveedores']);
+          this.nuevoProveedor = {};
+          this.getProveedores();
+          this.modalCloseAdd.nativeElement.click();
+        }),
+        catchError((error) => {
+          console.error('Error al agregar proveedor:', error);
+          this.errorAgregarProveedor = true;
+          setTimeout(() => {
+            this.errorAgregarProveedor = false;
+          }, 5000);
+          return of(error);
+        })
+      )
+      .subscribe();
   }
 
   modificarProveedor(): void {
-    this.providersService.modificarProveedor(this.proveedor).pipe(
-      tap(() => {
-        this.router.navigate(['/proveedores']);
-        this.getProveedores();
-        this.modalCloseUpdate.nativeElement.click();
-      }),
-      catchError((error) => {
-        console.error('Error al modificar proveedor:', error);
-        this.errorModificarProveedor = true;
-        setTimeout(() => {
-          this.errorModificarProveedor = false;
-        }, 5000);
-        return of(error);
-      })
-    ).subscribe();
+    this.providersService
+      .modificarProveedor(this.proveedor)
+      .pipe(
+        tap(() => {
+          this.router.navigate(['/proveedores']);
+          this.getProveedores();
+          this.modalCloseUpdate.nativeElement.click();
+        }),
+        catchError((error) => {
+          console.error('Error al modificar proveedor:', error);
+          this.errorModificarProveedor = true;
+          setTimeout(() => {
+            this.errorModificarProveedor = false;
+          }, 5000);
+          return of(error);
+        })
+      )
+      .subscribe();
   }
-  abrirModalModificacion(proveedorId: string, proveedorNombre: string, proveedorDireccion: string, proveedorTelefono: string, proveedorCorreo: string) {
+  abrirModalModificacion(
+    proveedorId: string,
+    proveedorNombre: string,
+    proveedorDireccion: string,
+    proveedorTelefono: string,
+    proveedorCorreo: string
+  ) {
     this.proveedor.id = proveedorId;
     this.proveedor.nombre = proveedorNombre;
     this.proveedor.direccion = proveedorDireccion;
     this.proveedor.telefono = proveedorTelefono;
     this.proveedor.email = proveedorCorreo;
   }
-
 }
