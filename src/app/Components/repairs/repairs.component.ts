e05af77b31c791d45d89a7569292800c26017fc0
STPR2001 <santiagoperron2016@gmail.com>
2024-08-14 23:40:16 -0300

    .

--- src/app/Components/repairs/repairs.component.ts
+++ src/app/Components/repairs/repairs.component.ts
@@ -481,7 +481,7 @@ export class RepairsComponent implements OnInit {
       format: [163, 460], // Ajusta el tamaño según sea necesario
     });
 
-    const lineHeight = 6; // Ajusta el interlineado según sea necesario
+    const lineHeight = 8; // Ajusta el interlineado según sea necesario
     let y = 10;
 
     pdf.setFontSize(10); // Tamaño de la fuente
@@ -569,6 +569,9 @@ export class RepairsComponent implements OnInit {
     y += 2;
 
     // Firma
+    pdf.text('Firma: _________________________', 2, y);
+    pdf.text('Firma: _________________________', 2, y);
+    pdf.text('Firma: _________________________', 2, y);
     pdf.text('Firma: _________________________', 2, y);
     y += lineHeight;
 
